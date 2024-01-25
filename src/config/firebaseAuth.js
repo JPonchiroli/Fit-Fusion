eaad94d6822b6d9d355bb25460827ff65991053d
João PonchirolI <joaopedroponqueroli@gmail.com>
2024-01-25 14:40:39 -0300

    Adding Home Screen

--- src/config/firebaseAuth.js
+++ src/config/firebaseAuth.js
@@ -1,8 +1,9 @@
-import { ref, set } from "firebase/database"
+import { getDatabase, ref, set, onValue, get } from "firebase/database"
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 import { app } from "./firebaseConfig";
 
 const Auth = getAuth(app);
+const database = getDatabase(app);
 
 export async function registerUser(email, senha, nomeCompleto) {
   try {
@@ -37,5 +38,48 @@ export const login = async (email, senha) => {
     return response.user;
   } catch (error) {
     throw error;
+  }
+};
+
+
+export const recuperaTreinosDoUsuario = async (uid, setTreinosDoUsuario) => {
+  try {
+    const snapshot = await ref(getDatabase(), `treinos/${uid}`);
+    onValue(snapshot, (data) => {
+      if (data.exists()) {
+        const treinosUsuarioData = data.val();
+        const treinosUsuarioArray = Object.keys(treinosUsuarioData).map((treinoUid) => ({
+          treinoUid,
+          ...treinosUsuarioData[treinoUid],
+        }));
+        setTreinosDoUsuario(treinosUsuarioArray);
+      } else {
+        console.log('Nenhum treino encontrado para o usuário.');
+      }
+    });
+  } catch (error) {
+    console.error('Erro ao recuperar treinos do usuário:', error);
+  }
+};
+
+export const recuperaInfoUsuario = async (uid, setUserInfo) => {
+  try {
+    const usuarioRef = ref(database, `usuarios/${uid}`);
+    const usuarioSnapshot = await get(usuarioRef);
+
+    if (usuarioSnapshot.exists()) {
+      const usuarioInfo = usuarioSnapshot.val();
+
+      setUserInfo((prevUserInfo) => ({
+        ...prevUserInfo,
+        usuario: usuarioInfo.nomeCompleto,
+        kg: usuarioInfo.kg,
+        altura: usuarioInfo.altura,
+      }));
+    } else {
+      console.log('Nenhum dado encontrado para o usuário.');
+    }
+  } catch (error) {
+    console.error('Erro ao recuperar informações do nó usuarios:', error);
   }
 };
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,231 @@
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { recuperaTreinosDoUsuario, recuperaInfoUsuario } from "../../config/firebaseAuth";
+import { auth as firebaseAuth } from "../../config/firebaseConfig";
+import { Feather } from "@expo/vector-icons";
 
-export default function Home() {
-  return(
-    <Text>Home</Text>
-  )
+export default function Home({ navigation }) {
+  const [userInfo, setUserInfo] = useState(null);
+  const [treinosDoUsuario, setTreinosDoUsuario] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firebaseAuth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUserInfo(user);
+
+        // Substitua o código abaixo para usar as novas funções
+        recuperaInfoUsuario(user.uid, setUserInfo);
+
+        // Substitua a chamada da função recuperaTreinosDoUsuario
+        recuperaTreinosDoUsuario(user.uid, setTreinosDoUsuario);
+      }
+    });
+
+    return () => {
+      // Use a instância de autenticação diretamente de firebaseAuth
+      const treinosRef = firebaseAuth.database().ref(`treinos/${firebaseAuth.currentUser.uid}`);
+      treinosRef.off('value');
+    };
+  }, []);
+
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        style={{
+          width: '100%',
+          height: '100%',
+        }}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 1, y: 0 }}
+        colors={['#000', '#0f0f0f', 'rgba(15, 15, 15, 0.88)', '#121212', '#252525']}
+      >
+
+        <LinearGradient
+          style={{
+            width: '100%',
+            height: 110,
+
+            borderRadius: 10,
+            justifyContent: 'center', // Align content in the center vertically
+            padding: 20, // Add some padding for better appearance
+          }}
+          start={{ x: 0, y: 1 }}
+          end={{ x: 1, y: 0 }}
+          colors={['rgba(245,90,110,1)', 'rgba(244,72,94,1)', 'rgba(219,64,84,1)']}
+        >
+          <View style={styles.header}>
+            <Image source={require('./../../img/BODY_IMG.png')} style={styles.headerImage} />
+          </View>
+        </LinearGradient>
+
+        <View style={styles.treinoss}>
+          <Text style={styles.titleSection}>Meus Treinos</Text>
+          <Text style={styles.subTitleSection}>Aqui está listado todos os seus treinos</Text>
+          <View style={styles.divider}></View>
+          <ScrollView
+            horizontal={true} // Enable horizontal scrolling
+            style={styles.scrollViewMenu}
+          >
+            {treinosDoUsuario.map((treino) => (
+
+              <TouchableOpacity
+                style={styles.touchableItem}
+                key={treino.uid}
+                onPress={() => navigation.navigate('TreinoSelecionado', { treino })}
+              >
+                <ImageBackground
+                  source={require("./../../img/treinoHome.png")}
+                  style={styles.backgroundImage}
+                  imageStyle={styles.imageStyle}
+                >
+                  <LinearGradient
+                    style={styles.linearGradientOverlay}
+                    colors={['rgba(9, 9, 9, 9)', 'transparent']} // Inverta as cores aqui
+                    start={{ x: 0, y: 1 }}
+                    end={{ x: 1, y: 0 }}
+                  >
+                    <Text style={styles.titleCardSubtitle}>A seguir:</Text>
+                    <Text style={styles.titleCard}>{treino.nomeTreino}</Text>
+                  </LinearGradient>
+
+                </ImageBackground>
+
+
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        </View>
+
+
+        <View style={styles.treinoss}>
+          <Text style={styles.titleSection}>Minha conta</Text>
+          <Text style={styles.subTitleSection}>Aqui está o status de sua conta</Text>
+          <View style={styles.divider}></View>
+          <View style={styles.AcountContainer}>
+
+            <Feather
+              style={{ marginTop: 10 }}
+              name="user"
+              size={80}
+              color={"rgba(255, 57, 83, 1)"}
+
+            />
+            {userInfo && (
+              <>
+                <View style={styles.AcountTextContainer}>
+                  <Text style={styles.title0}>Nome: {userInfo.usuario}</Text>
+                  <Text style={styles.title0}>Peso: {userInfo.kg} kg</Text>
+                  <Text style={styles.title0}>Altura: {userInfo.altura} m</Text>
+                </View>
+              </>
+            )}
+          </View>
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000'
+  },
+  imageStyle: {
+    borderRadius: 10, // Aplique o borderRadius aqui
+  },
+  AcountContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: 'center'
+
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginVertical: 10,
+    borderWidth: 4,
+    borderColor: 'rgba(255, 57, 83, 1)',
+  },
+  AcountTextContainer: {
+    display: "flex",
+    flexDirection: "column",
+    marginLeft: 10,
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover', // ou 'contain' dependendo do seu requisito
+  },
+  header: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    paddingTop: 20
+  },
+  headerImage: {
+    width: '25%',
+    height: '100%',
+    marginBottom: 20,
+  },
+  titleSection: {
+    color: '#fff',
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  subTitleSection: {
+    color: '#d4d4d4',
+    fontSize: 16,
+
+  },
+  linearGradientOverlay: {
+    position: 'absolute',
+    bottom: 0, // Ajuste conforme necessário
+    left: 0,   // Ajuste conforme necessário
+    width: '100%',
+    padding: 30, // Ajuste conforme necessário
+  },
+  treinoss: {
+    padding: 20,
+    margin: 10,
+  },
+  title0: {
+    color: "#FFF",
+  },
+  divider: {
+    borderBottomColor: '#d4d4d4', // Set the color of the divider line
+    borderBottomWidth: 0.2,     // Set the thickness of the divider line
+    marginVertical: 10,       // Adjust the vertical spacing as needed
+  },
+  touchableItem: {
+    background: 'transparent',
+    borderRadius: 10,
+    margin: 5,
+    height: 200,
+    width: 300,
+    justifyContent: "center",
+    alignItems: 'center',
+  },
+  scrollViewMenu: {
+    flexDirection: 'row',
+  },
+  titleCard: {
+    color: '#d6d6d6',
+    fontSize: 18,
+    position: 'absolute',
+    bottom: 10, // ajuste conforme necessário
+    left: 10,  // ajuste conforme necessário
+  },
+  titleCardSubtitle: {
+    color: '#FFF',
+    fontSize: 18,
+    position: 'absolute',
+    bottom: 30, // ajuste conforme necessário
+    left: 10,  // ajuste conforme necessário
+  }
+});
